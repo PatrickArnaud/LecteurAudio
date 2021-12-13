efb38c577fc4973166326f65bdad325cda884ff5
PatrickArnaud <patrick.arnaud34470@gmail.com>
2021-12-13 17:23:15 +0100

    correctifs

--- main.js
+++ main.js
@@ -14,6 +14,11 @@ let canvasHTML = document.querySelector('#canvas');
 let color = document.querySelector('#colorBar');
 let cssHTML = document.querySelector('#css');
 let visuelHTML = document.querySelector('#visuel');
+let styleHTML = document.querySelector('#styleBar');
+let styleScript = document.querySelector('#style');
+
+
+
 
 ///////FONCTION ACTIVE
 
@@ -180,7 +185,7 @@ function showDuration() {
         let total = "--mn--sec";
         totalTimeHTML.textContent = total;
     } else {
-       
+
         let total = calc + "mn" + calcSplit + "sec";
         totalTimeHTML.textContent = total;
     }
@@ -197,55 +202,75 @@ function currentDuration() {
 
 
 
-// ///////Visuel
-// function visu() {
-//     let context = new AudioContext();
-//     let src = context.createMediaElementSource(player);
-//     let analyser = context.createAnalyser();
-//     let canvas = document.getElementById("canvas");
-//     canvas.width = window.innerWidth;
-//     canvas.height = window.innerHeight;
-//     let ctx = canvas.getContext("2d");
-//     src.connect(analyser);
-//     analyser.connect(context.destination);
-//     analyser.fftSize = 256;
-//     let bufferLength = analyser.frequencyBinCount;
-//     let dataArray = new Uint8Array(bufferLength);
-//     let WIDTH = canvas.width;
-//     let HEIGHT = canvas.height;
-//     let barWidth = (WIDTH / bufferLength);
-//     let barHeight;
-//     let x = 0;
-//     function renderFrame() {
-//         requestAnimationFrame(renderFrame);
-//         x = 0;
-//         let changeColor = color.value;
-//         analyser.getByteFrequencyData(dataArray);
-//         ctx.fillStyle = "#1d1c1c";
-//         ctx.fillRect(0, 0, WIDTH, HEIGHT);
-//         for (let i = 0; i < bufferLength; i++) {
-//             barHeight = dataArray[i];
-//             let testColor = color.value;
-//             if (testColor < 50) {
-//                 let r = changeColor / 2 + barHeight + (25 * (i / bufferLength));
-//                 let g = 256 - volume.value;
-//                 let b = changeColor;
-//                 ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-//                 ctx.fillRect(x, HEIGHT - barHeight * 2.5, barWidth, barHeight);
-//                 x += barWidth + 3;
-//             } else {
-//                 let r = changeColor / 2 + barHeight + (25 * (i / bufferLength));
-//                 let g = 0 + volume.value;
-//                 let b = changeColor + 100;
-//                 ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-//                 ctx.fillRect(x, HEIGHT - barHeight * 2.5, barWidth, barHeight);
-//                 x += barWidth;
-//             }
-//         }
-//     }
-//     player.play();
-//     renderFrame();
-// };
+///////Visuel
+function visu() {
+    let context = new AudioContext();
+    let src = context.createMediaElementSource(player);
+    let analyser = context.createAnalyser();
+    let canvas = document.getElementById("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    let ctx = canvas.getContext("2d");
+    src.connect(analyser);
+    analyser.connect(context.destination);
+    analyser.fftSize = 256;
+    let bufferLength = analyser.frequencyBinCount;
+    let dataArray = new Uint8Array(bufferLength);
+    let WIDTH = canvas.width;
+    let HEIGHT = canvas.height;
+    let barWidth = (WIDTH / bufferLength);
+    let barHeight;
+    let x = 0;
+    function renderFrame() {
+        requestAnimationFrame(renderFrame);
+        x = 0;
+        let changeColor = color.value;
+        analyser.getByteFrequencyData(dataArray);
+        ctx.fillStyle = "#1d1c1c";
+        ctx.fillRect(0, 0, WIDTH, HEIGHT);
+        for (let i = 0; i < bufferLength; i++) {
+            barHeight = dataArray[i];
+            let testStyle = styleHTML.value;
+            if (testStyle == 1) {
+                let testColor = color.value;
+                if (testColor < 50) {
+                    let r = changeColor / 2 + barHeight + (25 * (i / bufferLength));
+                    let g = 256 - volume.value;
+                    let b = changeColor;
+                    ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+                    ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
+                    x += barWidth + 2;
+                } else {
+                    let r = changeColor / 2 + barHeight + (25 * (i / bufferLength));
+                    let g = 0 + volume.value;
+                    let b = changeColor + 100;
+                    ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+                    ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
+                    x += barWidth + 2;
+                }
+            } else {
+                let testColor = color.value;
+                if (testColor < 50) {
+                    let r = changeColor / 2 + barHeight + (25 * (i / bufferLength));
+                    let g = 256 - volume.value;
+                    let b = changeColor;
+                    ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+                    ctx.fillRect(x, HEIGHT - barHeight, 10, 10);
+                    x += barWidth + 2;
+                } else {
+                    let r = changeColor / 2 + barHeight + (25 * (i / bufferLength));
+                    let g = 0 + volume.value;
+                    let b = changeColor + 100;
+                    ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+                    ctx.fillRect(x, HEIGHT - barHeight, 10, 10);
+                    x += barWidth + 2;
+                }
+            }
+        }
+    }
+    player.play();
+    renderFrame();
+};
 
 //////LISTENER
 
@@ -285,6 +310,17 @@ color.addEventListener('input', (e) => {
     }
 });
 
+// //////TEST STYLE
+// styleHTML.addEventListener('input', (e) => {
+
+//     if (styleHTML.value < 2) {
+//         style.src = "visuUn.js"
+//         console.log("test");
+//     } else {
+//         style.src = "visuDeux.js"
+//         console.log("test");
+//     }
+// });
 
 ///////////MUTE
 muteHTML.addEventListener('click', (e) => {
